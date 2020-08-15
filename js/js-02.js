8b193b5589db1c31fec717a08eee71ee85dc68e4
Nazar100 <leshc005@umn.edu>
2020-08-15 11:09:48 +0300

    task 2

---
+++ js/js-02.js
@@ -0,0 +1,31 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+  message = message.split(" ");
+  price = message.length * pricePerWord;
+  message = message.join(" ");
+  console.log(`Цена за ${message} будет ${price} кредитов`);
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
