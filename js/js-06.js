af75dc682a03e17a28bf01f84065295ba2830074
Nazar100 <leshc005@umn.edu>
2020-08-15 12:31:07 +0300

    working

---
+++ js/js-06.js
@@ -0,0 +1,12 @@
+let input = prompt("введите число");
+const numbers = [];
+let total = 0;
+do {
+  input = Number(input);
+  numbers.push(input);
+  for (number of numbers) {
+    total += number;
+  }
+} while (input);
+
+console.log(total);
