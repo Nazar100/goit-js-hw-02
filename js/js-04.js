1433ae417009c91ca76b528b36d6fe5970251167
Nazar100 <leshc005@umn.edu>
2020-08-15 11:41:36 +0300

    task 4

---
+++ js/js-04.js
@@ -0,0 +1,28 @@
+const formatString = function (string) {
+  string = string.split("");
+
+  if (string.length <= 40) {
+    return string.join("");
+  } else {
+    string.splice(39);
+    string = string.join("");
+
+    return `${string}...`;
+  }
+};
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// вернется оригинальная строка
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// вернется форматированная строка
+
+console.log(formatString("Curabitur ligula sapien."));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
+// вернется форматированная строка
